61a213703504719f70da7f21aab6099785d8106b
Hasan <blackbee08@gmail.com>
2021-07-16 00:25:12 +0700

    Added exception and extension base.

---
+++ src/domain/common/Agoda.HotelManagement.Common/Exceptions/ApplicationErrorCodes.cs
@@ -0,0 +1,36 @@
+﻿using Agoda.HotelManagement.Common.Extensions;
+using Agoda.HotelManagement.Common.Resources;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agoda.HotelManagement.Common.Exceptions
+{
+    public sealed class ApplicationErrorCodes : StringEnum
+    {
+        public ApplicationErrorCodes(string value) : base(value)
+        {
+        }
+        public const string EmptyName = "EMPTY_CITY_NAME";
+        public const string DatabaseError = "DATABASE_CONFIGURATION_ERROR";
+        public const string InternalSeverError = "INTERNAL_SERVER_ERROR";
+        public const string NotFound = "NOT_FOUND";
+
+        public static string GetMessage(string value)
+        {
+            switch (value)
+            {
+                case EmptyName:
+                    return ErrorMessage.EmptyName;
+                case DatabaseError:
+                    return ErrorMessage.DatabaseError;
+                case InternalSeverError:
+                    return ErrorMessage.InternalSeverError;
+                case NotFound:
+                    return ErrorMessage.NotFound;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(value), value, null);
+            }
+        }
+    }
+}
---
+++ src/domain/common/Agoda.HotelManagement.Common/Exceptions/ApplicationException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Agoda.HotelManagement.Common.Exceptions
+{
+    public class ApplicationException
+    {
+        public ApplicationException()
+        {
+            ErrorCode = string.Empty;
+            Data = new ErrorData();
+        }
+        public ApplicationException(string errorCode, ErrorData errorData)
+        {
+            ErrorCode = errorCode;
+            Data = new ErrorData(errorData.Field, errorData.Message);
+        }
+
+        [JsonPropertyName("error_code")]
+        public string ErrorCode { get; set; }
+
+        [JsonPropertyName("data")]
+        public ErrorData Data { get; set; }
+    }
+}

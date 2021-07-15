af95084e82f4aa1214dcfc01d62700340318aff8
Hasan <blackbee08@gmail.com>
2021-07-15 23:58:33 +0700

    Added swagger to the controller.

---
+++ src/domain/core/Agoda.HotelManagement.Api/Controllers/HotelController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Agoda.HotelManagement.Api.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        public HotelController() 
+        {
+
+        }
+
+        [HttpGet("city/{name}")]
+        public IActionResult GetHotelsByCity(string name)
+        {
+            return StatusCode(StatusCodes.Status200OK, null);
+        }
+
+        [HttpGet("room/{type}")]
+        public IActionResult GetHotelsByRoom(string type)
+        {
+            return StatusCode(StatusCodes.Status200OK, null);
+        }
+    }
+}
--- src/domain/core/Agoda.HotelManagement.Api/Startup.cs
+++ src/domain/core/Agoda.HotelManagement.Api/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +27,8 @@ namespace Agoda.HotelManagement.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddApiVersioning();
+            ConfigureSwaggerServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +48,29 @@ namespace Agoda.HotelManagement.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Rate API");
+
+            });
+        }
+
+        private static void ConfigureSwaggerServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    {
+                        Title = "Swagger API",
+                        Description = "Rate API",
+                        Version = "v1"
+                    });
+
             });
         }
     }
